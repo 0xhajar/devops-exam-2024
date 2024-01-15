18f9da31a29f6245ec787019447a400ba26d5477
0xhajar <hajar.haouat@student.vinci.be>
2024-01-15 16:56:33 +0100

    refactoring : function isValid

--- src/services/validations.js
+++ src/services/validations.js
@@ -1,20 +1,32 @@
 // TODO: Refactor validation function to follow the defined validations rules
 const isValid = (gamertag) => {
-    if(gamertag.length >= 8 && 
-        (gamertag.includes('&') || 
-         gamertag.includes('$') || 
-         gamertag.includes('è') || 
-         gamertag.includes('!') || 
-         gamertag.includes('§') || 
-         gamertag.includes('à') || 
-         gamertag.includes('_'))) {
-            for(let i = 0; i < gamertag.length; i++) {
-                if(!isNaN(gamertag[i])) {
-                    return true;
-                }
-            }
+    if(atLeast8Characters(gamertag) && atLeastOneSpecialCharacter(gamertag) && atLeastOneNumber(gamertag)) {
+        return true;
     }
     return false;
 };
+
+function atLeast8Characters(gamertag) {
+    return gamertag.length >= 8;
+}
+
+function atLeastOneSpecialCharacter(gamertag) {
+    return gamertag.includes('&') || 
+        gamertag.includes('$') || 
+        gamertag.includes('è') || 
+        gamertag.includes('!') || 
+        gamertag.includes('§') || 
+        gamertag.includes('à') || 
+        gamertag.includes('_');
+}
+
+function atLeastOneNumber(gamertag) {
+    for(let i = 0; i < gamertag.length; i++) {
+        if(!isNaN(gamertag[i])) {
+            return true;
+        }
+    }
+    return false;
+}
 
 exports.isValid = isValid;
